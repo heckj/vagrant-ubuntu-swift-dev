e2bd6ab11e4dcaf93b48ad5e164bc35361cb8212
Joe Heck <heckj@mac.com>
2017-08-17 10:47:38 -0500

    updating to Ubuntu 16.10 and latest dev images

--- update_swift_snapshot.py
+++ update_swift_snapshot.py
@@ -8,11 +8,11 @@ import subprocess
 import os
 
 # swift 3.1 branch: curl https://swift.org/builds/swift-3.1-branch/ubuntu1604/latest-build.yml
-url = "https://swift.org/builds/development/ubuntu1604/latest-build.yml"
+url = "https://swift.org/builds/development/ubuntu1610/latest-build.yml"
 # Get latest snapshot name.
 data = yaml.load(urllib2.urlopen(url).read())
 # FIXME: We shouldn't need to do this, it should be available in the API.
-latestSnapshot = data["download"].replace("-ubuntu16.04.tar.gz", "")
+latestSnapshot = data["download"].replace("-ubuntu16.10.tar.gz", "")
 snapshot = url.rsplit('/', 1)[0] + '/' + latestSnapshot + '/' + data["download"]
 
 #print data
@@ -26,4 +26,4 @@ else:
         exit(1)
 # Call docker build:
 # docker build -t swiftpm-docker-1604 --build-arg SNAPSHOT=swift-DEVELOPMENT-SNAPSHOT-2016-11-28-a-ubuntu16.04.tar.gz .
-subprocess.call(["docker", "build", "-t", "swiftpm-docker-1604", "--build-arg", "SNAPSHOT=%s" % data["download"], "."])
+subprocess.call(["docker", "build", "-t", "swiftpm-docker-1610", "--build-arg", "SNAPSHOT=%s" % data["download"], "."])
